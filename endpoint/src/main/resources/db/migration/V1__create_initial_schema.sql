c6ddd9022d8529203a1ebbe5925d398eb24c8253
Gonçalo Amaro <17440@stu.ipbeja.pt>
2022-04-12 00:37:17 +0100

    Fix flyway, it works on my desktop

--- endpoint/src/main/resources/db/migration/V1__create_initial_schema.sql
+++ endpoint/src/main/resources/db/migration/V1__create_initial_schema.sql
@@ -1,25 +1,25 @@
 CREATE TABLE "webshop"
 (
-    "webshop_id"    BIGSERIAL    NOT NULL,
-    "handle"       VARCHAR(32)  NOT NULL UNIQUE,
-    "url"          VARCHAR(128) NOT NULL UNIQUE,
-    "interest_rate" SMALLINT     NOT NULL DEFAULT '20',
-    "a"            FLOAT        NOT NULL,
-    "b"            FLOAT        NOT NULL,
-    "c"            FLOAT        NOT NULL,
-    "currency"     VARCHAR(3)   NOT NULL DEFAULT 'EUR',
-    "run_jobs"      BOOLEAN      NOT NULL DEFAULT 'true',
-    "multi_supply"  BOOLEAN      NOT NULL DEFAULT 'false',
-    CONSTRAINT "webshop_pk" PRIMARY KEY ("webshop_id")
+	"webshopId"    BIGSERIAL    NOT NULL,
+	"handle"       VARCHAR(32)  NOT NULL UNIQUE,
+	"url"          VARCHAR(128) NOT NULL UNIQUE,
+	"interestRate" SMALLINT     NOT NULL DEFAULT '20',
+	"A"            FLOAT        NOT NULL,
+	"B"            FLOAT        NOT NULL,
+	"C"            FLOAT        NOT NULL,
+	"currency"     VARCHAR(3)   NOT NULL DEFAULT 'EUR',
+	"runJobs"      BOOLEAN      NOT NULL DEFAULT 'true',
+	"multiSupply"  BOOLEAN      NOT NULL DEFAULT 'false',
+	CONSTRAINT "webshop_pk" PRIMARY KEY ("webshopId")
 );
 
-CREATE TABLE "webshopemails"
+CREATE TABLE "webshopEmails"
 (
-    "address_id" BIGSERIAL    NOT NULL,
-    "webshop_id" BIGINT       NOT NULL,
-    "address"   VARCHAR(128) NOT NULL,
-    CONSTRAINT "webshopemails_pk" PRIMARY KEY ("address_id")
+	"addressId" BIGSERIAL    NOT NULL,
+	"webshopId" BIGINT       NOT NULL,
+	"address"   VARCHAR(128) NOT NULL,
+	CONSTRAINT "webshopEmails_pk" PRIMARY KEY ("addressId")
 );
 
-ALTER TABLE "webshopemails"
-    ADD CONSTRAINT "webshopemails_fk0" FOREIGN KEY ("webshop_id") REFERENCES "webshop" ("webshop_id");
+ALTER TABLE "webshopEmails"
+	ADD CONSTRAINT "webshopEmails_fk0" FOREIGN KEY ("webshopId") REFERENCES "webshop" ("webshopId");
